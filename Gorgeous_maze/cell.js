78669f583e634df0a3fbe93e2cb6fef265f2c791
fanbyprinciple <fanbyprinciple@gmail.com>
2019-07-05 00:10:05 +0530

    honeybomb

--- Gorgeous_maze/cell.js
+++ Gorgeous_maze/cell.js
@@ -2,35 +2,74 @@ function Cell(i,j,left,right,up,down){
   
   this.i = i
   this.j = j
-  this.color = (random(255))
+  this.color = 255
   this.left = left
   this.right = right
   this.up = up
   this.down = down
+  this.visited = false
   
 }
 
 Cell.prototype.show = function(cells){
   var x = this.i*w
   var y = this.j*w
-  //stroke(255)
-  //noFill()
-  //rect(x,y,w,w)
-  if(!this.up){
-    stroke(this.color)
-    line(x,y,x+w,y)
-  }
-  if(!this.right){
-    stroke(this.color)
-    line(x+w,y,x+w,y+w)
-  }
-  if(!this.down){
-    stroke(this.color)
-    line(x+w,y+w,x,y+w)
-  }
-  if(!this.left){
-    stroke(this.color)
-    line(x,y,x,y+w)
+  
+  stroke(255)
+  
+  if (this.visited){
+  
+  fill(255, 204, 0)
+   rect(x,y,w,w)
   }
   
+  //rect(x,y,w,w)
+  if(!this.up){
+    //stroke(this.color)
+    line(x  ,  y  ,x+w  ,y)
+  }
+  if(!this.right){
+    //stroke(this.color)
+    line(x+w  ,y  ,x+w  ,y+w)
+  }
+  if(!this.down){
+    //stroke(this.color)
+    line(x+w  ,y+w  ,x  ,y+w)
+  }
+  if(!this.left){
+    //stroke(this.color)
+    line(x  ,y  ,x  ,y+w)
+  }
+  
+}
+
+Cell.prototype.checkNeighbhors =  function(){
+  neighbhors = []
+  
+  var topCell = cells[index(this.i,this.j-1)]
+  var rightCell = cells[index(this.i+1,this.j)]
+  var bottomCell = cells[index(this.i,this.j+1)]
+  var leftCell = cells[index(this.i-1,this.j)]
+  
+  if(topCell && !topCell.visited ){
+   neighbhors.push(topCell) 
+  }
+  if(leftCell && !leftCell.visited){
+   neighbhors.push(leftCell) 
+  }
+  if(bottomCell && !bottomCell.visited){
+   neighbhors.push(bottomCell) 
+  }
+  if(rightCell && !rightCell.visited){
+   neighbhors.push(rightCell) 
+  }
+  
+  console.log(neighbhors)
+  
+  if(neighbhors.length >0){
+    var r = floor(random(0,neighbhors.length))
+    return neighbhors[r]
+  } else {
+    return undefined
+  }
 }
--- Gorgeous_maze/sketch.js
+++ Gorgeous_maze/sketch.js
@@ -1,6 +1,14 @@
 var cols, rows
 var w = 40
 var cells = []
+var currentCell
+
+function index(i,j) {
+  
+  if(i<0 || j<0 || i >rows-1 || j >rows-1)
+    return -1
+  return i+j*cols
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -9,14 +17,190 @@ function setup() {
   
   for(var i = 0 ; i < rows; i++)
     for(var j = 0 ; j < cols; j++){
-    var cell = new Cell(i,j,Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2))
+    //var cell = new Cell(i,j,Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2))
+    var cell = new Cell(i,j,1,1,1,1)
     cells.push (cell)
     }
+  
+  currentCell = cells[0]
 }
 
 function draw() {
   background(51);
+  frameRate(5)
+  currentCell.visited = true
   for(var i = 0 ; i < cells.length; i++){
     cells[i].show()
   }
+  
+  var next = currentCell.checkNeighbhors()
+  if(next){
+    next.visited = true
+    currentCell = next
+  }
+  
+}var cols, rows
+var w = 40
+var cells = []
+var currentCell
+
+function index(i,j) {
+  
+  if(i<0 || j<0 || i >rows-1 || j >rows-1)
+    return -1
+  return i+j*cols
+}
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width/w)
+  rows = floor(height/w)
+  
+  for(var i = 0 ; i < rows; i++)
+    for(var j = 0 ; j < cols; j++){
+    //var cell = new Cell(i,j,Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2))
+    var cell = new Cell(i,j,1,1,1,1)
+    cells.push (cell)
+    }
+  
+  currentCell = cells[0]
+}
+
+function draw() {
+  background(51);
+  frameRate(5)
+  currentCell.visited = true
+  for(var i = 0 ; i < cells.length; i++){
+    cells[i].show()
+  }
+  
+  var next = currentCell.checkNeighbhors()
+  if(next){
+    next.visited = true
+    currentCell = next
+  }
+  
+}var cols, rows
+var w = 40
+var cells = []
+var currentCell
+
+function index(i,j) {
+  
+  if(i<0 || j<0 || i >rows-1 || j >rows-1)
+    return -1
+  return i+j*cols
+}
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width/w)
+  rows = floor(height/w)
+  
+  for(var i = 0 ; i < rows; i++)
+    for(var j = 0 ; j < cols; j++){
+    //var cell = new Cell(i,j,Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2))
+    var cell = new Cell(i,j,1,1,1,1)
+    cells.push (cell)
+    }
+  
+  currentCell = cells[0]
+}
+
+function draw() {
+  background(51);
+  frameRate(5)
+  currentCell.visited = true
+  for(var i = 0 ; i < cells.length; i++){
+    cells[i].show()
+  }
+  
+  var next = currentCell.checkNeighbhors()
+  if(next){
+    next.visited = true
+    currentCell = next
+  }
+  
+}var cols, rows
+var w = 40
+var cells = []
+var currentCell
+
+function index(i,j) {
+  
+  if(i<0 || j<0 || i >rows-1 || j >rows-1)
+    return -1
+  return i+j*cols
+}
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width/w)
+  rows = floor(height/w)
+  
+  for(var i = 0 ; i < rows; i++)
+    for(var j = 0 ; j < cols; j++){
+    //var cell = new Cell(i,j,Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2))
+    var cell = new Cell(i,j,1,1,1,1)
+    cells.push (cell)
+    }
+  
+  currentCell = cells[0]
+}
+
+function draw() {
+  background(51);
+  frameRate(5)
+  currentCell.visited = true
+  for(var i = 0 ; i < cells.length; i++){
+    cells[i].show()
+  }
+  
+  var next = currentCell.checkNeighbhors()
+  if(next){
+    next.visited = true
+    currentCell = next
+  }
+  
+}var cols, rows
+var w = 40
+var cells = []
+var currentCell
+
+function index(i,j) {
+  
+  if(i<0 || j<0 || i >rows-1 || j >rows-1)
+    return -1
+  return i+j*cols
+}
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width/w)
+  rows = floor(height/w)
+  
+  for(var i = 0 ; i < rows; i++)
+    for(var j = 0 ; j < cols; j++){
+    //var cell = new Cell(i,j,Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2),Math.floor(Math.random()*2))
+    var cell = new Cell(i,j,1,1,1,1)
+    cells.push (cell)
+    }
+  
+  currentCell = cells[0]
+}
+
+function draw() {
+  background(51);
+  frameRate(5)
+  currentCell.visited = true
+  for(var i = 0 ; i < cells.length; i++){
+    cells[i].show()
+  }
+  
+  var next = currentCell.checkNeighbhors()
+  if(next){
+    next.visited = true
+    currentCell = next
+  }
+  
 }

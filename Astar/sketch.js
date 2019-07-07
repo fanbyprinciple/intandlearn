e6866ebb1ce00cdcaa00e7abdadb5d255a43d998
fanbyprinciple <fanbyprinciple@gmail.com>
2019-07-07 14:51:38 +0530

    Astar: need to add heuristics

--- Astar/sketch.js
+++ Astar/sketch.js
@@ -8,18 +8,42 @@ var closedSet = []
 var start,end
 var w,h
 
+function removeFromArray(array, element){
+  for(var i = array.length-1; i >0 ; i--){
+    if(array[i]== element){
+      array.splice(i,1)
+    }
+  }
+} 
+
 function Spot(i,j){
-  this.x = i
-  this.y = j
+  this.i = i
+  this.j = j
+  
+  this.neighbhors= []
   
   this.f = 0
   this.g = 0
   this.h = 0
   
+  this.addNeighbhors =  function(grid){
+    if(i< cols-1){
+      this.neighbhors.push(grid[this.i+1][this.j])}
+    
+    if(i>0){
+      this.neighbhors.push(grid[this.i-1][this.j])}
+    
+    if(j < rows-1){
+      this.neighbhors.push(grid[this.i][this.j+1])}
+    
+    if(j > 0){
+      this.neighbhors.push(grid[this.i][this.j-1])}
+  }
+  
   this.show = function(col){
     fill(col)
     noStroke()
-    rect(this.x*w,this.y*h,w-1,h-1)
+    rect(this.i*w,this.j*h,w-1,h-1)
   }
 }
 
@@ -36,11 +60,18 @@ function setup() {
     //console.log(grid[l])
   }
   
-  for(var i = 0 ; i < cols; i++){
-  for (var j = 0 ; j  < rows; j++){
+  for(let i = 0 ; i < cols; i++){
+  for (let j = 0 ; j  < rows; j++){
     grid[i][j] = new Spot (i,j)
     }
   }
+  
+  for(let i = 0 ; i < cols; i++){
+  for (let j = 0 ; j  < rows; j++){
+    grid[i][j].addNeighbhors(grid)
+    }
+  }
+  
   
   start = grid[0][0]
   end = grid[cols-1][rows-1]
@@ -51,7 +82,7 @@ function setup() {
   
   
   
-  //console.log(grid)
+  console.log(grid)
   
   
 }
@@ -59,25 +90,61 @@ function setup() {
 function draw() {
   background(0);
   
-  /*if(openSet.length > 0){
+  if(openSet.length > 0){
     //keep going
+    var winner = 0
+    for(var i =0; i < openSet.length; i++){
+      if(openSet[i].f<openSet[winner].f){
+        winner = i
+      }
+    }
+    
+    var current = openSet[winner]
+    
+    if(current === end){
+      console.log("done!")
+    }
+    
+    //openSet.remove(current)
+    removeFromArray(openSet, current)
+    closedSet.push(current)
+    
+    var neighbhors = current.neighbhors
+    for(let i = 0; i, neighbhors.length; i++){
+    var neighbhor = neighbhors[i]
+    
+    if(!closedSet.includes(neighbhor)){
+    var tempG = current.g +1
+    
+    if(openSet.includes(neighbhor)){
+      if(tempG < neighbhor.g) {neighbhor.h=g = tempG}
+    } else {
+      neighbhor.g = tempG
+      openSet.push(neighbhor) 
+    }
+      
+      // add heiristics
+    
+    }
+    }
+    
   } else {
     //no solution
-  }*/
+  }
   
-  for(var i = 0 ; i < cols; i++){
-  for (var j = 0 ; j  < rows; j++){
+  for(let i = 0 ; i < cols; i++){
+  for (let j = 0 ; j  < rows; j++){
     grid[i][j].show(color(255, 204, 0))
     
     }
   }
   
-  for(var k =0 ; k <openSet.length; k++){
+  for(let k =0 ; k <openSet.length; k++){
     openSet[k].show('#0f0')
   }
   
-  for(var l=0; l <closedSet.length; l++){
-    closedSet.show('hsla(160, 100%, 50%, 0.5)')
+  for(let l=0; l <closedSet.length; l++){
+    closedSet[l].show('hsla(160, 100%, 50%, 0.5)')
   }
   
   

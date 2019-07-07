02217d2c25d1884470624c799582211899e4fadf
fanbyprinciple <fanbyprinciple@gmail.com>
2019-07-07 14:06:27 +0530

    Astar: started

---
+++ Astar/sketch.js
@@ -0,0 +1,72 @@
+var cols = 5
+var rows = 5
+var grid = new Array(cols)
+
+var openSet = []
+var closedSet = []
+
+var start,end
+var w,h
+
+function Spot(i,j){
+  this.x = i
+  this.y = j
+  
+  this.f = 0
+  this.g = 0
+  this.h = 0
+  
+  this.show = function(col){
+    fill(col)
+    noStroke()
+    rect(this.x*w,this.y*h,w,h-1)
+  }
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  w = width/cols
+  h = height/rows
+  
+  
+  for (var l  =0; l< cols; l++){
+    grid[l] = new Array(rows)
+  }
+  
+  for(var i = 0 ; i < cols; i++){
+  for (var j = 0 ; j  , rows; j++){
+    grid[i][j] = new Spot (i,j)
+    }
+  }
+  
+  start = grid[0][0]
+  end = grid[cols-1][rows-1]
+  
+  openSet.push(start)
+  
+  
+  
+  
+  
+  //console.log(grid)
+  
+  
+}
+
+function draw() {
+  background(0);
+  
+  /*if(openSet.length > 0){
+    //keep going
+  } else {
+    //no solution
+  }*/
+  
+  for(var i = 0 ; i < cols; i++){
+  for (var j = 0 ; j  , rows; j++){
+    grid[i][j].show(color(255, 204, 0))
+    }
+  }
+}

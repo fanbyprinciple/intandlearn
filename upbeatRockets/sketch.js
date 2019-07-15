8b8a68cbaf832467ccec9b168e7e095e26f7ad27
fanbyprinciple <fanbyprinciple@gmail.com>
2019-07-15 23:31:47 +0530

    upbeatRockets: added genes

--- upbeatRockets/sketch.js
+++ upbeatRockets/sketch.js
@@ -1,4 +1,5 @@
 var population
+var lifespan = 200
 
 function setup() {
   createCanvas(400, 400);
@@ -7,9 +8,17 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(0);
   population.run()
   
+}
+
+function DNA(){
+  this.genes = []
+  for (var i = 0; i < lifespan; i++){
+    this.genes[i] = p5.Vector.random2D() 
+    this.genes.setMag(0.1)
+  }
 }
 
 function Population(){
@@ -31,15 +40,20 @@ function Population(){
 
 function Rocket() {
   this.pos = createVector(width/2, height)
-  //this.vel = createVector(0,-1)
-  this.vel = p5.Vector.random2D()
+  this.vel = createVector()
+  //this.vel = p5.Vector.random2D()
   this.acc = createVector()
+  this.dna = new DNA()
+  this.count = 0 
   
   this.applyForce =  function(force){
     this.acc.add(force)
   }
   
   this.update =function() {
+    
+    this.applyForce(this.dna.genes[this.count])
+    this.count++
     this.vel.add(this.acc)
     this.pos.add(this.vel)
     this.acc.mult(0)
@@ -47,10 +61,12 @@ function Rocket() {
   
   this.show =  function() {
     push()
+    noStroke()
+    fill(255,150)
     translate(this.pos.x,this.pos.y)
     rotate(this.vel.heading())
     rectMode(CENTER)
-    rect(0,0,50,10)
+    rect(0,0,25,5)
     pop()
   }
   

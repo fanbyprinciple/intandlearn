c8bc5927642c25af99949cf0cbe4f1d9db56c149
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-07-24 03:07:15 +0530

    upbeatRockets: Genetic maxfit bug removed

--- upbeatRockets/sketch.js
+++ upbeatRockets/sketch.js
@@ -29,7 +29,9 @@ function draw() {
 
     
     if(count == lifespan){
-     population =new Population()
+     //population =new Population()
+     population.evaluate()
+     population.selection()
      count = 0
     }
 
@@ -40,11 +42,56 @@ function draw() {
 function Population() {
     this.rockets = []
     this.popsize = 100
+    this.matingPool = []
 
     for (let i=0;i < this.popsize; i++){
         this.rockets[i] = new Rocket()
     }
 
+    this.evaluate=function(){
+        
+        let maxfit =0
+        for (let i=0;i < this.popsize; i++){
+            this.rockets[i].calcFitness()
+            if(this.rockets[i].fitness > maxfit){
+                maxfit  = this.rockets[i].fitness
+                //console.log("maxfit not zero    ")
+            }
+        }
+
+        for (let i=0;i < this.popsize; i++){
+            this.rockets[i].fitness /= maxfit
+        }
+
+        // for(let i=0;i <this.popsize; ++i){
+        //     console.log(this.rockets[i])
+        // }
+        
+        this.matingPool = []
+
+        for (let i=0;i < this.popsize; i++){
+            
+            let n = this.rockets[i].fitness*100
+            //console.log("inside evaluate:", this.rockets[0].fitness)
+            for(let j =0; j <n ;j ++){
+                this.matingPool.push(this.rockets[i])
+            }
+        }
+    }
+
+    this.selection =function() {
+        let newRockets =[]
+        for (let i = 0;i<this.rockets.length; i++){
+            let parentA = random(this.matingPool).dna
+            let parentB = random(this.matingPool).dna
+            let child = parentA.crossover(parentB)
+            newRockets[i] = new Rocket(child)
+
+        }
+        this.rockets = newRockets
+        
+
+    }
 
     this.run = function() {
         for (let i=0;i < this.popsize; i++){
@@ -57,25 +104,56 @@ function Population() {
 
 }
 
-function DNA() {
+function DNA(genes) {
+    if(genes){
+        this.genes = genes
+    } else {
     this.genes=[]
     for(let i =0; i <lifespan ; ++i){
         this.genes[i] = p5.Vector.random2D()
         this.genes[i].setMag(0.1)
+        }
+    }
+    this.crossover = function(partner) {
+        let newgenes = []
+        let midpoint = floor(random(this.genes.length))
+        for (let i =0; i < this.genes.length; i++){
+            if (i >midpoint){
+                newgenes[i] = this.genes[i]
+            } else {
+                newgenes[i] = partner.genes[i]
+            }
+        }
+
+        return new DNA(newgenes)
     }
 
 
 
 }
 
-function Rocket() {
+function Rocket(dna) {
     this.pos = createVector(width/2,height)
     this.vel = createVector()
     this.acc = createVector()
+    if(dna){
+        this.dna = dna
+    } else {
     this.dna = new DNA()
+    }                                                                                                                            
+    this.fitness =0
 
     this.applyForce = function(force){
         this.acc.add(force)
+
+    }
+
+    this.calcFitness = function(){
+    
+        let d = dist(this.pos.x,this.pos.y,target.x,target.y)
+        this.fitness = map(d,0,width,width,0)
+        
+
 
     }
 

67ca7d4b46f08c282eb6fa8e25f5dd43d0a12dc1
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-07-25 22:57:06 +0530

    tsp_simple: the pink lineis the exhaustive shortest path

--- tsp_simple/sketch.js
+++ tsp_simple/sketch.js
@@ -2,15 +2,24 @@ var cities = []
 
 var totalCities
 
+var recordDistance
+
+var bestEver
+
 function setup (){ 
     createCanvas(400,300)
 
     totalCities = 10
 
+    bestEver = cities.slice()
+
     for(let i= 0; i<totalCities ;++i){
         let v = createVector(random(width),random(height))
         cities[i] = v
     }
+
+    let d = calcDistance(cities)
+    recordDistance = d
 }
 
 function draw(){
@@ -30,7 +39,25 @@ function draw(){
     }
     endShape()
 
+
+    stroke (255,0,255)
+    strokeWeight(4)
+    noFill ()
+    beginShape()
+    for(let i= 0; i<bestEver.length ;++i){
+        vertex(bestEver[i].x, bestEver[i].y)
+    }
+    endShape()
+
+
     swap(cities,floor (random(cities.length)), floor(random(cities.length)))
+
+    let d = calcDistance(cities)
+
+    if (d < recordDistance) {
+        recordDistance = d
+        bestEver = cities.slice()
+    }
 
 }
 
@@ -38,4 +65,15 @@ function swap (a, i,j){
     let temp = a[i]
     a[i] = a[j]
     a[j] = temp
+}
+
+function calcDistance(points){
+    let sum = 0
+
+    for(let i=0; i <points.length-1; ++i){
+        var d =dist(points[i].x, points[i].y,points[i+1].x,points[i+1].y)
+        sum += d
+    }
+
+    return sum
 }

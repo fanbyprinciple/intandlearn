408e99b575cf80a9e28b4f759f5db4383af67dfc
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2020-02-18 07:52:35 +0530

    tensorflow_js : created

---
+++ tensorflow_js/readme.md
@@ -0,0 +1,4 @@
+# Getting to know basics of tensorflow js
+
+### variables and operations in tensorflow.js
+![](variables_operations.png)
--- tensorflow_js/tensor.js
+++ tensorflow_js/tensor.js
@@ -6,9 +6,42 @@ function setup(){
         values[i] = random(0,100)
     }
 
-    const shape = [5,3] // 5 rows 3 columns
-    const data = tf.tensor2d(values, shape)
+    const shapeA = [5,3] // 5 rows 3 columns
+    const shapeB = [3,5] // transpose
 
-    //const data = tf.tensor([0,0,127.7,255,100,50,24,54],[2,2,2], 'int32')
-    data.print()
+    const a = tf.tensor2d(values, shapeA)
+    const b = tf.tensor2d(values,shapeB)
+
+    const tb = b.transpose()
+
+    // a.print()
+    b.print()
+    tb.print()
+
+    const c = tb.add(a)
+
+    console.log("Here comes the C")
+    c.print()
+
+    const d = a.mul([2])
+    console.log("Here comes the d")
+    d.print()
+
+    const e = a.matMul(b)
+    console.log("Here comes the e")
+    e.print()
+    // const vtense = tf.variable(tense)
+
+    // //const data = tf.tensor([0,0,127.7,255,100,50,24,54],[2,2,2], 'int32')
+    // tense.print()
+    // // tense.data().then(function(stuff){
+    // //     console.log(stuff)
+    // // }) // async
+
+    // console.log(tense.dataSync())
+    //  // same thing as above but is synchronous
+    // // console.log(tense.get(14)) //not working
+
+
+    // //tense.set(0,10)
 }

cbe8d659e19c22f3224e2e1edd4c95d1ecfe69bc
fanbyprinciple <fanbyprinciple@gmail.com>
2020-02-23 11:01:07 +0530

    tensorflow.js : quadratic regression done

--- tensorflow_js/poly_regression.js
+++ tensorflow_js/poly_regression.js
@@ -6,21 +6,24 @@
 let xvals = []
 let yvals = []
 
-let m,b 
+let a,b,c 
+
+let dragging = false
 
 const learningRate = 0.5
-const optimizer = tf.train.sgd(learningRate)
+const optimizer = tf.train.adam(learningRate)
 
 function setup(){
     createCanvas(200,200)
-    m = tf.variable(tf.scalar(random(1)))
-    b = tf.variable(tf.scalar(random(1)))
+    a = tf.variable(tf.scalar(random(-1,1)))
+    b = tf.variable(tf.scalar(random(-1,1)))
+    c = tf.variable(tf.scalar(random(-1,1)))
 }
 
 function predict(x){
     const xs = tf.tensor1d(x)
     //xs.print()
-    const ys = xs.mul(m).add(b)
+    const ys = xs.square().mul(a).add(xs.mul(b)).add(c)
     //ys.print()
     return ys
 }
@@ -32,61 +35,73 @@ function loss(pred,label){
 }
 
 function mousePressed(){
-    let x = map(mouseX,0,width,0,1)
-    let y = map(mouseY,0,height,1,0)
+    dragging = true
+}
 
-    xvals.push(x)
-    yvals.push(y)
+function mouseReleased(){
+    dragging = false
 }
 
 function draw(){
     background(0)
+    if (dragging){
+        let x = map(mouseX,0,width,-1,1)
+        let y = map(mouseY,0,height,1,-1)
 
-    // training magic
-    // handled by optimizer
-    if(xvals.length > 0){
-        tf.tidy(()=>{
-            // how the fuck is this line affecting m and b
-            optimizer.minimize(()=> loss(predict(xvals),yvals))
-            // console.log("m: ") 
-            // m.print()
-            // console.log("b: ") 
-            // b.print()
-        })
-            
+        xvals.push(x)
+        yvals.push(y)
+    } else {
+        // training magic
+        // handled by optimizer
+        if(xvals.length > 0){
+            tf.tidy(()=>{
+                // how the fuck is this line affecting m and b
+                optimizer.minimize(()=> loss(predict(xvals),yvals))
+                // console.log("m: ") 
+                // m.print()
+                // console.log("b: ") 
+                // b.print()
+            })
+                
+        }    
     }
+    
 
     //print the points
     strokeWeight(8)
     stroke(255)
     for(let i=0; i <xvals.length;++i){
-        let px = map(xvals[i],0,1,0,width)
-        let py = map(yvals[i],0,1,height,0)
+        let px = map(xvals[i],-1,1,0,width)
+        let py = map(yvals[i],-1,1,height,0)
         //console.log(xvals,yvals)
         point(px, py)
     }
 
     // testing the prediction function
-    let lineX = [0,1]
+    let curveX = []
+    for(let x = -1; x<1 ; x+=0.05){
+        curveX.push(x)
+    }
+
     let ys
 
-    tf.tidy(()=>{
-        if(xvals.length > 0){
-            ys = predict(lineX)
-    
-            // now drawing the line
-            let x1 = map(lineX[0],0,1,0,width)
-            let x2 = map(lineX[1],0,1,0,width)
+    // printing the curve
+    ys = tf.tidy(()=> predict(curveX))
+    let curveY = ys.dataSync()
+    ys.dispose()
             
-            let lineY = ys.dataSync()
+    beginShape()
+    noFill()
+    stroke(255)
+    strokeWeight(2)
+    for (let i=0; i<curveX.length; ++i){
+        let x = map(curveX[i], -1, 1, 0, width)
+        let y = map(curveY[i],1,-1,0,height)
+        vertex(x,y)
+    }
+    endShape()
+
     
-            let y1 = map(lineY[0],0,1,height,0)
-            let y2 = map(lineY[1],0,1,height,0)
-    
-            strokeWeight(2)
-            line(x1,y1,x2,y2)
-        }
-    })
     console.log("Number of tensors :", tf.memory().numTensors)
     
 }
--- tensorflow_js/readme.md
+++ tensorflow_js/readme.md
@@ -10,3 +10,9 @@
 https://www.youtube.com/watch?v=dLp10CFIvxI&list=PLRqwX-V7Uu6YIeVA3dNxbR9PYj4wV31oQ&index=5
 
 ![](lr_error.png)
+
+### quadratic regresssion with tensorflow.js
+
+https://www.youtube.com/watch?v=tIXDik5SGsI&list=PLRqwX-V7Uu6YIeVA3dNxbR9PYj4wV31oQ&index=6
+
+![](quad.png)
